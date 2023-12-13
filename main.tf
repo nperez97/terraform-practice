eb30c904d36913b82e2757703006d253cd41045a
nperez97 <nefi1997@gmail.com>
2023-12-12 21:10:27 -0700

    Update AWS AMI ID in main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-first-server" {
-    ami = "ami-016485166ec7fa705"
+    ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
 
     tags = {
