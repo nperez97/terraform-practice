cd0eec29e78fe831394345238a5bb2614dce05c3
nperez97 <nefi1997@gmail.com>
2023-12-15 19:43:44 -0700

    update read me; remove unused variables

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project Overview
+This project is meant to be a simple example of using terraform to create a single instance on AWS while also leveraging github action for testing.
--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key_terraform" {
-  description = "value of aws_access_key"
-  type        = string
-  default     = "<your aws_access_key>"
-}
-
-variable "aws_secret_key_terraform" {
-  description = "value of aws_secret_key"
-  type        = string
-  default     = "<your aws_secret_key>"
-}
-
 variable "ec2_instance_type" {
   description = "value of ec2_instance_type"
   type        = string
@@ -26,22 +14,4 @@ variable "ec2_ami" {
   description = "value of ec2_ami"
   type        = string
   default     = "ami-0c00eacddaea828c6"
-}
-
-variable "S3_BUCKET_NAME" {
-  description = "S3 bucket name"
-  type        = string
-  default     = "<bucket name>"
-}
-
-variable "PATH_TO_BACKUP" {
-  description = "Path to tfstate in S3 bucket"
-  type        = string
-  default     = "<path to tfstate in S3 bucket>"
-}
-
-variable "AWS_BUCKET_REGION" {
-  description = "AWS bucket region"
-  type        = string
-  default     = "<bucket region>"
 }
